aedcb56b0282229a779badce33410a076e2185d4
slightlydeviant <eklee1212@gmail.com>
2015-10-09 13:33:11 -0400

    added evaluation function

---
+++ scripts/evaluate_function.R
@@ -0,0 +1,9 @@
+# evaluate_function.R
+
+evaluate <- function(truth, fitted){
+  # Calculate the Root Mean Squared Percentage Error
+  stopifnot(length(truth) = length(fitted))
+  len <- length(fitted)
+  rmspe <- sqrt(len * sum(((truth - fitted) / truth)^2))
+  return(rmspe)
+}
