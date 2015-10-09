7ee3220f0beec27ae5763b8dff16800af486a326
slightlydeviant <eklee1212@gmail.com>
2015-10-09 14:14:34 -0400

    Fixed error in evaluate function

--- scripts/evaluate_function.R
+++ scripts/evaluate_function.R
@@ -2,7 +2,7 @@
 
 evaluate <- function(truth, fitted){
   # Calculate the Root Mean Squared Percentage Error
-  stopifnot(length(truth) = length(fitted))
+  stopifnot(length(truth) == length(fitted))
   len <- length(fitted)
   rmspe <- sqrt(len * sum(((truth - fitted) / truth)^2))
   return(rmspe)
