f8ab15c6a07d441e16927591e5b252bade4803c1
Over-achiever <dexkxo1@5CG508037Y.darden.com>
2015-10-23 12:12:21 -0400

    stole Erik's code

--- scripts/KOexplore.R
+++ scripts/KOexplore.R
@@ -1,4 +1,16 @@
-library(dadva)
-library(dplyr)
+# explore_variables.R
 
-store<-read.csv("data/store.csv")
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+
+store <- read.csv("data/store.csv")
+train <- read.csv("data/train.csv")
+test <- read.csv("data/test.csv")
+
+
+# Store Data -----
+
+
+# Training Data -----
+train$Date <- as.Date(train$Date, "%Y-%m-%d")
