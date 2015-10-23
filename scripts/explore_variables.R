8c6b649284d1299f97515ee60349f9d9c1f70702
slightlydeviant <eklee1212@gmail.com>
2015-10-23 12:51:53 -0400

    Deleted KOexplore.R after moving it to scripts/. Began exploring train data

--- scripts/explore_variables.R
+++ scripts/explore_variables.R
@@ -1,4 +1,5 @@
 # explore_variables.R
+# Purpose: Clean data, create new variables, plot and explore, transformations
 
 library(dplyr)
 library(tidyr)
@@ -10,7 +11,28 @@ test <- read.csv("data/test.csv")
 
 
 # Store Data -----
-
+names(store)
+head(store)
+str(store)
 
 # Training Data -----
+names(train)
+head(train)
+str(train)
+
+# change variable classes
+train$Store <- factor(train$Store)
 train$Date <- as.Date(train$Date, "%Y-%m-%d")
+train$Weekend <- factor(ifelse(train$DayOfWeek %in% c(6, 7), "Weekend", "Weekday"))
+train$DayOfWeek2 <- factor(train$DayOfWeek, levels = 1:7, 
+                           labels = c("Monday", "Tuesday", "Wednesday", 
+                                      "Thursday", "Friday", "Saturday", "Sunday"))
+train$Open <- factor(train$Open)
+train$Promo <- factor(train$Promo)
+train$SchoolHoliday <- factor(train$SchoolHoliday)
+str(train)
+
+summary(train)
+table(train$Store)  # Every store has 942 days, ie. no missing data
+
+
